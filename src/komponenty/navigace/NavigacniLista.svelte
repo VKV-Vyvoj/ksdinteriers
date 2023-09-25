1ccf21971807c8c3f82fe6cf96434f264f211492
Zdenek Krcil <krcilzdenek50@gmail.com>
2023-09-25 15:33:22 -0700

    ss

--- src/komponenty/navigace/NavigacniLista.svelte
+++ src/komponenty/navigace/NavigacniLista.svelte
@@ -1,23 +1,9 @@
-<nav class="bg-white w-full flex relative justify-between items-center mx-auto px-8 h-20">
-    <!-- Logo on the left -->
-    <div class="inline-flex">
-        <a class="_o6689fn" href="/">
-            <div class="hidden md:block">
-                <img src="https://firebasestorage.googleapis.com/v0/b/truhlarstvi-interiors.appspot.com/o/logo.png?alt=media&token=7eac34ad-d4e5-4a00-88c6-7f4062c9907d" alt="Logo" width="150" height="64">
-            </div>
-            <div class="block md:hidden">
-                <img src="https://firebasestorage.googleapis.com/v0/b/truhlarstvi-interiors.appspot.com/o/logo.png?alt=media&token=7eac34ad-d4e5-4a00-88c6-7f4062c9907d" alt="Logo" width="150" height="64">
-            </div>
-        </a>
-    </div>
+<nav class="bg-black w-full flex items-center mx-auto px-8 h-20">
 
     <!-- Centered links next to each other -->
-    <div class="flex justify-center">
-        <a class="mx-4" href="#">Domů</a>
-        <a class="mx-4" href="#">Realizace</a>
-        <a class="mx-4" href="#">Kontakt</a>
+    <div class="flex w-full justify-center">
+        <a class="mx-4 text-white" href="/">Domů</a>
+        <a class="mx-4 text-white" href="/relizace">Realizace</a>
+        <a class="mx-4 text-white" href="/kontakt">Kontakt</a>
     </div>
-
-    <!-- Right-side buttons -->
-    
 </nav>
