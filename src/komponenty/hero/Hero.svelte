480419866682ae318c8ff8a40cc735249a713513
Zdenek Krcil <krcilzdenek50@gmail.com>
2023-09-25 11:21:57 -0700

    mobile

--- src/komponenty/hero/Hero.svelte
+++ src/komponenty/hero/Hero.svelte
@@ -3,12 +3,47 @@
         <div class="font-light text-black sm:text-center sm:text-lg">
             <h2 class="text-center mb-4 text-4xl font-extrabold text-gray-900">Vítejte</h2>
             <p class="mb-4">Naše mistrovské kusy spojují tradici a moderní design, aby vaše domovy zazářily. Každý výrobek nese pečlivý výběr materiálů a vášeň, kterou do naší práce vkládáme. Nezáleží na stylu – rustikálním, moderním nebo na míru – jsme zde, abychom vaše představy proměnili v realitu. Vstupte do světa dřeva a objevte krásu truhlářského řemesla.</p>
-           
         </div>
         <div class="grid grid-cols-2 gap-4 mt-8">
-            <img class="w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/office-long-2.png" alt="office content 1">
-            <img class="mt-4 w-full lg:mt-10 rounded-lg" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/office-long-1.png" alt="office content 2">
+            <!-- Pomalejší animace pro první obrázek (zde použijeme animate-pulse s delší dobou trvání) -->
+            <div class="animate-pulse-slow">
+                <img class="w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/office-long-2.png" alt="office content 1">
+            </div>
+            <!-- Pomalejší animace pro druhý obrázek (zde použijeme animate-bounce s delší dobou trvání) -->
+            <div class="animate-bounce-slow">
+                <img class="mt-4 w-full lg:mt-10 rounded-lg" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/office-long-1.png" alt="office content 2">
+            </div>
         </div>
     </div>
 </section>
+
+<style>
+    /* CSS pro pomalé animace */
+    @keyframes pulse-slow {
+        0%, 100% {
+            opacity: 0.9;
+        }
+        50% {
+            opacity: 0.9;
+        }
+    }
+
+    @keyframes bounce-slow {
+        0%, 100% {
+            transform: translateY(0);
+        }
+        50% {
+            transform: translateY(-10px);
+        }
+    }
+
+    /* Použití pomalých animací */
+    .animate-pulse-slow {
+        animation: pulse-slow 4s infinite;
+    }
+
+    .animate-bounce-slow {
+        animation: bounce-slow 4s infinite;
+    }
+</style>
 
