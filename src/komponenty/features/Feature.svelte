e139516a9dbd8dd59e86b97ccc1ff40993a29617
Zdenek Krcil <krcilzdenek50@gmail.com>
2023-09-25 12:10:43 -0700

    sss

--- src/komponenty/features/Feature.svelte
+++ src/komponenty/features/Feature.svelte
@@ -1,38 +1,36 @@
-<div class="flex items-center gap-16 py-8 px-4 mx-auto max-w-screen-xl">
-    <div class="font-light text-black sm:text-center sm:text-lg w-1/2">
-        <h2 class="text-center mb-4 text-4xl font-extrabold text-gray-900">Co děláme?</h2>
-        <p class="mb-4">Naše truhlářské realizace jsou plné vášně a preciznosti. Vytváříme nejen kuchyně, ale i elegantní skříně a nábytkové kusy, které spojují funkci s estetikou. Každý projekt je jedinečný a pečlivě promyšlený do nejmenšího detailu. Vaše představy a potřeby jsou naším hlavním směrem, ať už se jedná o kuchyň, skříň nebo jiný nábytek. S námi získáte nejen praktičnost, ale i krásu do svého domova.</p>
-    </div>
+<div class="px-4 py-8 max-w-screen-xl mx-auto">
+    <div class="sm:flex sm:items-center sm:gap-16">
+        <div class="sm:w-1/2 sm:text-center sm:mb-8">
+            <h2 class="text-4xl font-extrabold text-gray-900">Co děláme?</h2>
+            <p class="mt-4">Naše truhlářské realizace jsou plné vášně a preciznosti. Vytváříme nejen kuchyně, ale i elegantní skříně a nábytkové kusy, které spojují funkci s estetikou. Každý projekt je jedinečný a pečlivě promyšlený do nejmenšího detailu. Vaše představy a potřeby jsou naším hlavním směrem, ať už se jedná o kuchyň, skříň nebo jiný nábytek. S námi získáte nejen praktičnost, ale i krásu do svého domova.</p>
+        </div>
 
-    <!-- První buňka vedle textu "Vítejte" -->
-    <div class="w-1/4">
-        <div class="mx-auto max-w-md overflow-hidden rounded-lg bg-white shadow">
-            <img src="https://firebasestorage.googleapis.com/v0/b/truhlarstvi-interiors.appspot.com/o/CC8BA99C-45D7-4C73-9C8D-B6519632FE2A.jpeg?alt=media&token=4fc42213-a50c-4636-a6ad-6844cfebbd8f" class="aspect-video w-full object-cover" alt="">
-            <div class="p-4">
-                <p class="mb-1 text-sm text-primary-500">• KSDINTERIORS •</p>
-                <h3 class="text-xl font-medium text-gray-900">Název realizace</h3>
+        <div class="sm:w-1/4 mb-8">
+            <div class="mx-auto max-w-md overflow-hidden rounded-lg bg-white shadow">
+                <img src="https://firebasestorage.googleapis.com/v0/b/truhlarstvi-interiors.appspot.com/o/CC8BA99C-45D7-4C73-9C8D-B6519632FE2A.jpeg?alt=media&token=4fc42213-a50c-4636-a6ad-6844cfebbd8f" class="w-full aspect-w-8 aspect-h-9 object-cover" alt="">
+                <div class="p-4">
+                    <p class="mb-1 text-sm text-primary-500">• KSDINTERIORS •</p>
+                    <h3 class="text-xl font-medium text-gray-900">Název realizace</h3>
+                </div>
             </div>
         </div>
-    </div>
 
-    <!-- Druhá buňka vedle textu "Vítejte" -->
-    <div class="w-1/4">
-        <div class="mx-auto max-w-md overflow-hidden rounded-lg bg-white shadow">
-            <img src="https://firebasestorage.googleapis.com/v0/b/truhlarstvi-interiors.appspot.com/o/CC8BA99C-45D7-4C73-9C8D-B6519632FE2A.jpeg?alt=media&token=4fc42213-a50c-4636-a6ad-6844cfebbd8f" class="aspect-video w-full object-cover" alt="">
-            <div class="p-4">
-                <p class="mb-1 text-sm text-primary-500">• KSDINTERIORS •</p>
-                <h3 class="text-xl font-medium text-gray-900">Název realizace</h3>
+        <div class="sm:w-1/4 mb-8">
+            <div class="mx-auto max-w-md overflow-hidden rounded-lg bg-white shadow">
+                <img src="https://firebasestorage.googleapis.com/v0/b/truhlarstvi-interiors.appspot.com/o/CC8BA99C-45D7-4C73-9C8D-B6519632FE2A.jpeg?alt=media&token=4fc42213-a50c-4636-a6ad-6844cfebbd8f" class="w-full aspect-w-8 aspect-h-9 object-cover" alt="">
+                <div class="p-4">
+                    <p class="mb-1 text-sm text-primary-500">• KSDINTERIORS •</p>
+                    <h3 class="text-xl font-medium text-gray-900">Název realizace</h3>
+                </div>
             </div>
         </div>
     </div>
 </div>
-
 
 <div class="flex flex-wrap -mx-4">
-    <!-- První buňka dole -->
-    <div class="w-1/2 md:w-1/4 px-4 mb-8">
+    <div class="w-full sm:w-1/4 md:w-1/8 px-4 mb-8">
         <div class="mx-auto max-w-md overflow-hidden rounded-lg bg-white shadow">
-            <img src="https://firebasestorage.googleapis.com/v0/b/truhlarstvi-interiors.appspot.com/o/CC8BA99C-45D7-4C73-9C8D-B6519632FE2A.jpeg?alt=media&token=4fc42213-a50c-4636-a6ad-6844cfebbd8f" class="aspect-video w-full object-cover" alt="">
+            <img src="https://firebasestorage.googleapis.com/v0/b/truhlarstvi-interiors.appspot.com/o/CC8BA99C-45D7-4C73-9C8D-B6519632FE2A.jpeg?alt=media&token=4fc42213-a50c-4636-a6ad-6844cfebbd8f" class="w-full aspect-w-8 aspect-h-9 object-cover" alt="">
             <div class="p-4">
                 <p class="mb-1 text-sm text-primary-500">• KSDINTERIORS •</p>
                 <h3 class="text-xl font-medium text-gray-900">Název realizace</h3>
@@ -40,10 +38,9 @@
         </div>
     </div>
 
-    <!-- Druhá buňka dole -->
-    <div class="w-1/2 md:w-1/4 px-4 mb-8">
+    <div class="w-full sm:w-1/4 md:w-1/8 px-4 mb-8">
         <div class="mx-auto max-w-md overflow-hidden rounded-lg bg-white shadow">
-            <img src="https://firebasestorage.googleapis.com/v0/b/truhlarstvi-interiors.appspot.com/o/CC8BA99C-45D7-4C73-9C8D-B6519632FE2A.jpeg?alt=media&token=4fc42213-a50c-4636-a6ad-6844cfebbd8f" class="aspect-video w-full object-cover" alt="">
+            <img src="https://firebasestorage.googleapis.com/v0/b/truhlarstvi-interiors.appspot.com/o/CC8BA99C-45D7-4C73-9C8D-B6519632FE2A.jpeg?alt=media&token=4fc42213-a50c-4636-a6ad-6844cfebbd8f" class="w-full aspect-w-8 aspect-h-9 object-cover" alt="">
             <div class="p-4">
                 <p class="mb-1 text-sm text-primary-500">• KSDINTERIORS •</p>
                 <h3 class="text-xl font-medium text-gray-900">Název realizace</h3>
@@ -51,10 +48,9 @@
         </div>
     </div>
 
-    <!-- Třetí buňka dole -->
-    <div class="w-1/2 md:w-1/4 px-4 mb-8">
+    <div class="w-full sm:w-1/4 md:w-1/8 px-4 mb-8">
         <div class="mx-auto max-w-md overflow-hidden rounded-lg bg-white shadow">
-            <img src="https://firebasestorage.googleapis.com/v0/b/truhlarstvi-interiors.appspot.com/o/CC8BA99C-45D7-4C73-9C8D-B6519632FE2A.jpeg?alt=media&token=4fc42213-a50c-4636-a6ad-6844cfebbd8f" class="aspect-video w-full object-cover" alt="">
+            <img src="https://firebasestorage.googleapis.com/v0/b/truhlarstvi-interiors.appspot.com/o/CC8BA99C-45D7-4C73-9C8D-B6519632FE2A.jpeg?alt=media&token=4fc42213-a50c-4636-a6ad-6844cfebbd8f" class="w-full aspect-w-16 aspect-h-9 object-cover" alt="">
             <div class="p-4">
                 <p class="mb-1 text-sm text-primary-500">• KSDINTERIORS •</p>
                 <h3 class="text-xl font-medium text-gray-900">Název realizace</h3>
@@ -62,14 +58,14 @@
         </div>
     </div>
 
-    <!-- Čtvrtá buňka dole -->
-    <div class="w-1/2 md:w-1/4 px-4 mb-8">
+    <div class="w-full sm:w-1/4 md:w-1/8 px-4 mb-8">
         <div class="mx-auto max-w-md overflow-hidden rounded-lg bg-white shadow">
-            <img src="https://firebasestorage.googleapis.com/v0/b/truhlarstvi-interiors.appspot.com/o/CC8BA99C-45D7-4C73-9C8D-B6519632FE2A.jpeg?alt=media&token=4fc42213-a50c-4636-a6ad-6844cfebbd8f" class="aspect-video w-full object-cover" alt="">
+            <img src="https://firebasestorage.googleapis.com/v0/b/truhlarstvi-interiors.appspot.com/o/CC8BA99C-45D7-4C73-9C8D-B6519632FE2A.jpeg?alt=media&token=4fc42213-a50c-4636-a6ad-6844cfebbd8f" class="w-full aspect-w-16 aspect-h-9 object-cover" alt="">
             <div class="p-4">
-                <p class="mb-1 text-sm text-primary-500">• KSDINTERIORS • </p>
+                <p class="mb-1 text-sm text-primary-500">• KSDINTERIORS •</p>
                 <h3 class="text-xl font-medium text-gray-900">Název realizace</h3>
             </div>
         </div>
     </div>
 </div>
+
