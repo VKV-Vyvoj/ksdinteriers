55a8a68bbbe1f24f91a3e32937411e601a4dcff1
Zdenek Krcil <krcilzdenek50@gmail.com>
2023-09-25 11:17:24 -0700

    mobile

--- src/komponenty/navigace/NavigacniLista.svelte
+++ src/komponenty/navigace/NavigacniLista.svelte
@@ -1,31 +1,23 @@
-<!-- component -->
-<nav class=" bg-white w-full flex relative justify-between items-center mx-auto px-8 h-20">
-<!-- logo -->
-<div class="inline-flex items-center">
-    <a class="_o6689fn" href="/">
-        <div class="hidden md:block">
-            <img src="https://firebasestorage.googleapis.com/v0/b/truhlarstvi-interiors.appspot.com/o/Brand%20initial%20Simple%20Logo.png?alt=media&token=3c1bb499-656c-42a2-8251-3c00907075f3" alt="Logo" style="display: block; width: auto; max-height: 200px;">
-        </div>
-    </a>
-</div>
-<!-- end logo -->
+<nav class="bg-white w-full flex relative justify-between items-center mx-auto px-8 h-20">
+    <!-- Logo on the left -->
+    <div class="inline-flex">
+        <a class="_o6689fn" href="/">
+            <div class="hidden md:block">
+                <img src="https://firebasestorage.googleapis.com/v0/b/truhlarstvi-interiors.appspot.com/o/logo.png?alt=media&token=7eac34ad-d4e5-4a00-88c6-7f4062c9907d" alt="Logo" width="150" height="64">
+            </div>
+            <div class="block md:hidden">
+                <img src="https://firebasestorage.googleapis.com/v0/b/truhlarstvi-interiors.appspot.com/o/logo.png?alt=media&token=7eac34ad-d4e5-4a00-88c6-7f4062c9907d" alt="Logo" width="150" height="64">
+            </div>
+        </a>
+    </div>
 
-    <!-- search bar -->
-    <div class="flex space-x-2">
-        <button class="flex items-center flex-grow-0 flex-shrink px-2 relative w-16 sm:w-28 bg-white text-black hover:bg-black hover:text-white rounded-full transition-colors duration-300 ease-in-out" type="button">
-          <span class="block flex-grow flex-shrink overflow-hidden text-sm sm:text-base">Domů</span>
-        </button>
-        <button class="flex items-center flex-grow-0 flex-shrink px-2 relative w-16 sm:w-28 bg-white text-black hover:bg-black hover:text-white rounded-full transition-colors duration-300 ease-in-out" type="button">
-          <span class="block flex-grow flex-shrink overflow-hidden text-sm sm:text-base">Realizace</span>
-        </button>
-        <button class="flex items-center flex-grow-0 flex-shrink px-2 relative w-16 sm:w-28 bg-white text-black hover:bg-black hover:text-white rounded-full transition-colors duration-300 ease-in-out" type="button">
-          <span class="block flex-grow flex-shrink overflow-hidden text-sm sm:text-base">O nás</span>
-        </button>
-        <button class="flex items-center flex-grow-0 flex-shrink px-2 relative w-16 sm:w-28 bg-white text-black hover:bg-black hover:text-white rounded-full transition-colors duration-300 ease-in-out" type="button">
-          <span class="block flex-grow flex-shrink overflow-hidden text-sm sm:text-base">Kontakt</span>
-        </button>
-      </div>
-    <!-- end search bar -->
+    <!-- Centered links next to each other -->
+    <div class="flex justify-center">
+        <a class="mx-4" href="#">Domů</a>
+        <a class="mx-4" href="#">Realizace</a>
+        <a class="mx-4" href="#">Kontakt</a>
+    </div>
 
+    <!-- Right-side buttons -->
     
 </nav>
