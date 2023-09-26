d126c62f4ee7da3323657cf28ef879ef898f4df2
Zdenek Krcil <krcilzdenek50@gmail.com>
2023-09-26 06:31:24 -0700

    ss

--- src/komponenty/social/SocialMedia.svelte
+++ src/komponenty/social/SocialMedia.svelte
@@ -6,12 +6,11 @@
       </div>
     </a>
 
-    <a href="www.instagram.com/truhlarstvi_ksdinteriors" class="mb-2 inline-block rounded-2xl px-6 py-2.5 text-xs font-medium uppercase leading-normal text-white shadow-md transition duration-150 ease-in-out hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transform hover:scale-105" style="background-color: #000000">
-        <div class="flex items-center"> <!-- Přidáno obalový div -->
-           
-          <span class="ml-2">Instagram</span> <!-- Přidáno nápis tlačítka -->
-        </div>
-      </a>
+   <a href="https://www.instagram.com/truhlarstvi_ksdinteriors" target="_blank" rel="noopener noreferrer" class="mb-2 inline-block rounded-2xl px-6 py-2.5 text-xs font-medium uppercase leading-normal text-white shadow-md transition duration-150 ease-in-out hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transform hover:scale-105" style="background-color: #000000">
+  <div class="flex items-center">
+    <span class="ml-2">Instagram</span>
+  </div>
+</a>
 
       <a href="tel:+420702133193" class="mb-2 inline-block rounded-2xl px-6 py-2.5 text-xs font-medium uppercase leading-normal text-white shadow-md transition duration-150 ease-in-out hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transform hover:scale-105" style="background-color: #000000">
         <div class="flex items-center"> <!-- Přidáno obalový div -->
