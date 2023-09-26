36fde8165f0e1b136b4b1e39c761a2d191f8a926
Zdenek Krcil <krcilzdenek50@gmail.com>
2023-09-26 06:56:02 -0700

    inst

--- src/komponenty/galerie/Galerie.svelte
+++ src/komponenty/galerie/Galerie.svelte
@@ -1,55 +1,55 @@
 <div class="container mx-auto p-4">
-    <div class="grid grid-cols-1 md:grid-cols-4 gap-4">
-        <div>
+    <div class="grid grid-cols-1 md:grid-cols-4 gap-4">   
+        <div class="flex justify-center items-center">
             <img
               class="h-64 max-w-full rounded-lg"
               src="https://firebasestorage.googleapis.com/v0/b/truhlarstvi-interiors.appspot.com/o/8ece09c6-9dd3-4cfc-9a8d-42df278026b3.jpeg?alt=media&token=d8c1a004-991f-4719-8175-a804de248da0"
               alt=""
             />
           </div>
-          <div>
+          <div class="flex justify-center items-center">
             <img
               class="h-64 max-w-full rounded-lg"
               src="https://firebasestorage.googleapis.com/v0/b/truhlarstvi-interiors.appspot.com/o/0de62a3a-252f-46c4-baa7-229e160af21b.jpeg?alt=media&token=23540102-d233-4934-99c7-a4c7ed63c78a"
               alt=""
             />
           </div>
-          <div>
+          <div class="flex justify-center items-center">
             <img
               class="h-64 max-w-full rounded-lg"
               src="https://firebasestorage.googleapis.com/v0/b/truhlarstvi-interiors.appspot.com/o/433fdb79-8710-4cb5-85ef-3d9193464f76.jpeg?alt=media&token=bae7a53a-8498-4fa4-a5f6-ba12cc5d99a1"
               alt=""
             />
           </div>
-          <div>
+          <div class="flex justify-center items-center">
             <img
               class="h-64 max-w-full rounded-lg"
               src="https://firebasestorage.googleapis.com/v0/b/truhlarstvi-interiors.appspot.com/o/2e7d5ae3-ccf4-43b3-83fc-d757e171c713.jpeg?alt=media&token=c2247569-1f74-4c63-9ac8-8b21d00a6965"
               alt=""
             />
           </div>
-          <div>
+          <div class="flex justify-center items-center">
             <img
               class="h-64 max-w-full rounded-lg"
               src="https://firebasestorage.googleapis.com/v0/b/truhlarstvi-interiors.appspot.com/o/797fb041-2f7f-4b60-bae3-0160eef98f3c.jpeg?alt=media&token=54fa2730-20db-4088-9735-13d63f4eba47"
               alt=""
             />
           </div>
-          <div>
+          <div class="flex justify-center items-center">
             <img
               class="h-64 max-w-full rounded-lg"
               src="https://firebasestorage.googleapis.com/v0/b/truhlarstvi-interiors.appspot.com/o/A9B7151A-5A1E-4605-A404-BE9C97551D2B.jpeg?alt=media&token=1616dd7e-858c-4178-8122-c2a02a2e6fa7"
               alt=""
             />
           </div>
-          <div>
+          <div class="flex justify-center items-center">
             <img
               class="h-64 max-w-full rounded-lg"
               src="https://firebasestorage.googleapis.com/v0/b/truhlarstvi-interiors.appspot.com/o/7679fa6b-5322-4d9a-a76c-0ce2ffa89824.jpeg?alt=media&token=43f29b54-2997-421e-80af-dad8ccfab980"
               alt=""
             />
           </div>
-          <div>
+          <div class="flex justify-center items-center">
             <img
               class="h-64 max-w-full rounded-lg"
               src="https://firebasestorage.googleapis.com/v0/b/truhlarstvi-interiors.appspot.com/o/D32F1C4E-516C-4B27-8CE5-24C779CB62DE.jpeg?alt=media&token=bd1dd52b-79c6-424b-b402-025b671563e9"
@@ -58,4 +58,3 @@
           </div>
     </div>
   </div>
-  
