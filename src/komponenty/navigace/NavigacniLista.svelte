3a8b18383ee63edd4692e8547572e05bf473ddfb
Zdenek Krcil <krcilzdenek50@gmail.com>
2023-09-26 03:57:01 -0700

    ss

--- src/komponenty/navigace/NavigacniLista.svelte
+++ src/komponenty/navigace/NavigacniLista.svelte
@@ -1,5 +1,4 @@
 <nav class="bg-black w-full flex items-center mx-auto px-8 h-20">
-
     <!-- Centered links next to each other -->
     <div class="flex w-full justify-center">
         <a class="mx-4 text-white" href="/">Domů</a>
@@ -10,3 +9,13 @@
         <a class="mx-4 text-white" href="/kontakt">Kontakt</a>
     </div>
 </nav>
+
+<style>
+/* Media Query pro mobilní zařízení s maximální šířkou 640px */
+@media (max-width: 640px) {
+    /* Zde můžete upravit styly odkazů tak, aby byly viditelné a responzivní na malých obrazovkách */
+    .mx-4 {
+        margin: 8px; /* Nastavit vhodné odsazení pro odkazy */
+    }
+}
+</style>
