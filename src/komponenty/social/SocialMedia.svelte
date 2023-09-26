0018913dea0d2ced5544aa96cfe4510bf2613cc9
Zdenek Krcil <krcilzdenek50@gmail.com>
2023-09-26 06:21:38 -0700

    interiors

--- src/komponenty/social/SocialMedia.svelte
+++ src/komponenty/social/SocialMedia.svelte
@@ -6,7 +6,7 @@
       </div>
     </a>
 
-    <a href="instagram.com/truhlarstvi_ksdinteriors" class="mb-2 inline-block rounded-2xl px-6 py-2.5 text-xs font-medium uppercase leading-normal text-white shadow-md transition duration-150 ease-in-out hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transform hover:scale-105" style="background-color: #000000">
+    <a href="www.instagram.com/truhlarstvi_ksdinteriors" class="mb-2 inline-block rounded-2xl px-6 py-2.5 text-xs font-medium uppercase leading-normal text-white shadow-md transition duration-150 ease-in-out hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transform hover:scale-105" style="background-color: #000000">
         <div class="flex items-center"> <!-- Přidáno obalový div -->
            
           <span class="ml-2">Instagram</span> <!-- Přidáno nápis tlačítka -->
